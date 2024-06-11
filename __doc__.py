5a93b4ca3003f5775055920f7214dfbf4d1ace8e
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-06-11 21:29:26 +0530

    hello

---
+++ __doc__.py
@@ -0,0 +1,14 @@
+def multiply(a,b):
+    '''hello mr Gaurav 
+    what are you doing'''
+    return a*b
+print(multiply.__doc__)
+print(multiply(4,6))
+multiply(4,5)#not give any result
+
+# def multiply(a,b):
+#     '''hello mr Gaurav 
+#     what are you doing'''
+#     print(a*b)
+# print(multiply.__doc__)
+# multiply(3,5)
