62cc4d7591c428adab62556378d86bbffacd0925
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-06-17 14:28:13 +0530

    iterator

---
+++ iterator.py
@@ -0,0 +1,51 @@
+# lis = [1,2,3,4,5,6,7,8]
+# print(type(lis))
+# for i in lis:
+#     print(i)
+# x = iter(lis)
+# print(type(x))
+# print(x)
+# y = "GAuRAV"
+# z = iter(y)
+# print(dir(y))
+# print(dir(z))
+
+
+# x = "PYTHON TIWARI"
+# y = iter(x)#iterator
+# # print(type(y))
+# # print(y)
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+# print(next(y))
+
+# class xyz():
+#     def __iter__(self):
+#         self.a = 1
+#         return self
+#     def __next__(self):
+#         x = self.a
+#         self.a +=1
+#         return x
+# p1 = xyz()
+# myiter = iter(p1)
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+
+def xyz():
+    i = 1
+    while(i<=10):
+        return i
+print(xyz())
+print(type(xyz))
+
+def xyz():
+    i = 1
+    while(i<=10):
+        yield i
+print(xyz())
+print(type(xyz))
