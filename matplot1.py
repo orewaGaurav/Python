4d6d68f6a3de5a756ce1e8d2b6f425eae661a2b5
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-06-21 09:45:09 +0530

    Mattplot

--- matplot1.py
+++ matplot1.py
@@ -1,8 +1,16 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# xpoints = np.array([0, 6])
+# ypoints = np.array([0, 250])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+x = np.random.normal(170, 10, 250)
 
-plt.plot(xpoints, ypoints)
+plt.hist(x)
 plt.show()
